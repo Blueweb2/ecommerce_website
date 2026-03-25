aacadf008d61260f4c52efac946dbde2ffee07cb
navaskm <navaskm228@gmail.com>
2026-03-24 22:56:39 -0700

    created brands section

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import dynamic from "next/dynamic";
 
 const Navbar = dynamic(() => import("@/components/shared/Navbar"));
 const HeroSection = dynamic(() => import("@/components/home/HeroSection"));
+const BrandSection = dynamic(() => import("@/components/home/BrandSection"));
 
 const page = () => {
   return (
     <>
-      <Navbar/>
-      <HeroSection/>
+      <Navbar />
+      <HeroSection />
+      <BrandSection />
     </>
-  )
+  );
 };
 
 export default page;
