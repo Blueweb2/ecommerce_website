0de91e4bfb626cc6c71cc2d7745d2503a9a4bd74
navaskm <navaskm228@gmail.com>
2026-03-31 02:11:05 -0700

    updated related products section

--- components/details/RelatedProducts.tsx
+++ components/details/RelatedProducts.tsx
@@ -1,50 +1,115 @@
 "use client";
-import { useState } from "react";
 
-const products = [
+import { useState, useEffect } from "react";
+import { Handbag } from "lucide-react";
+
+type Products = {
+  name: string;
+  price: string;
+  image: string;
+  description: string;
+  id: number;
+}
+
+const relatedProducts = [
   {
     id: 1,
     name: "GOLD HOOP EARRINGS",
-    desc: "Bold yet minimal starter hugg hoops",
+    description: "Bold yet minimal starter hugg hoops",
     price: "₹11,200",
     image: "/home/categorysection/category-one.png",
   },
   {
     id: 2,
     name: "CHARM BRACELET",
-    desc: "Playful yet elegant charm bracelet",
+    description: "Playful yet elegant charm bracelet",
     price: "₹8,700",
     image: "/home/categorysection/category-one.png",
   },
   {
     id: 3,
     name: "GEMSTONE STATEMENT PENDANT",
-    desc: "Vibrant centerpiece with detailing",
+    description: "Vibrant centerpiece with detailing",
     price: "₹16,300",
     image: "/home/categorysection/category-one.png",
   },
   {
     id: 4,
     name: "MINIMAL GOLD BAND",
-    desc: "Simple and timeless everyday ring",
+    description: "Simple and timeless everyday ring",
     price: "₹8,900",
     image: "/home/categorysection/category-one.png",
   },
   {
     id: 5,
     name: "MINIMAL GOLD BAND",
-    desc: "Simple and timeless everyday ring",
+    description: "Simple and timeless everyday ring",
     price: "₹8,900",
     image: "/home/categorysection/category-one.png",
+  },
+];
+
+const resentViewProducts = [
+  {
+    id: 1,
+    name: "GOLD HOOP EARRINGS",
+    description: "Bold yet minimal starter hugg hoops",
+    price: "₹11,200",
+    image: "/home/herosection/hero-right-top.png",
+  },
+  {
+    id: 2,
+    name: "CHARM BRACELET",
+    description: "Playful yet elegant charm bracelet",
+    price: "₹8,700",
+    image: "/home/herosection/hero-right-top.png",
+  },
+  {
+    id: 3,
+    name: "GEMSTONE STATEMENT PENDANT",
+    description: "Vibrant centerpiece with detailing",
+    price: "₹16,300",
+    image: "/home/herosection/hero-right-top.png",
+  },
+  {
+    id: 4,
+    name: "MINIMAL GOLD BAND",
+    description: "Simple and timeless everyday ring",
+    price: "₹8,900",
+    image: "/home/herosection/hero-right-top.png",
+  },
+  {
+    id: 5,
+    name: "MINIMAL GOLD BAND",
+    description: "Simple and timeless everyday ring",
+    price: "₹8,900",
+    image: "/home/herosection/hero-right-top.png",
   },
 ];
 
 export default function RelatedProducts() {
 
   const [activeTab, setActiveTab] = useState("like");
+  const [products, setProducts] = useState<Products[]>();
+
+  useEffect(() => {
+    
+    switch (activeTab) {
+      case "like":
+        setProducts(relatedProducts)
+        break;
+
+      case "recent":
+        setProducts(resentViewProducts)
+        break;
+    
+      default:
+        break;
+    }
+  },[activeTab, products]);
 
   return (
-    <section className="max-w-7xl mx-auto pb-10 pl-5 lg:pl-0">
+    <section className="max-w-7xl lg:mx-auto pb-10 pl-5 lg:pl-0">
       
       {/* Tabs */}
       <div className="flex justify-center gap-8 text-sm tracking-wide mt-10 lg:mt-0 mb-8">
@@ -73,7 +138,7 @@ export default function RelatedProducts() {
 
       {/* Products */}
       <div className="flex overflow-x-auto gap-6 scrollbar-hide">
-        {products.map((item) => (
+        {products?.map((item) => (
           <div key={item.id} className="group cursor-pointer flex-shrink-0 w-[150px] lg:w-[290px]">
             
             {/* Image */}
@@ -85,17 +150,17 @@ export default function RelatedProducts() {
               />
 
               {/* Bag Icon  */}
-              <div className="absolute bottom-0 right-0 w-7 h-7 flex items-center justify-center bg-gray-100">
-                👜
+              <div className="absolute bottom-0 right-0 w-7 h-7 flex items-center justify-center bg-white/30 backdrop-blur-md text-white">
+                  <Handbag size={16} />
+                </div>
               </div>
-            </div>
 
             {/* Content */}
             <div className="mt-3">
               <h3 className="text-xs font-semibold tracking-wide">
                 {item.name}
               </h3>
-              <p className="text-xs text-gray-500 mt-">{item.desc}</p>
+              <p className="text-xs text-gray-500 mt-">{item.description}</p>
               <p className="text-sm mt-2">{item.price}</p>
             </div>
 
